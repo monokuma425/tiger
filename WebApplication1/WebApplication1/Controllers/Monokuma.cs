8041214e31d6ec09dc87b5074a314e1d278bb5df
Ирина <irusik4r@mail.ru>
2016-03-20 13:14:09 +0600

    веб

---
+++ WebApplication1/WebApplication1/Controllers/Monokuma.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MonokumaController : Controller
+    {
+        //
+        // GET: /monokuma/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Metod()
+        {
+            return "blablabla";
+        }
+        public string Welcome(string name, int count)
+        {
+            return string.Format("Hello, {0}, count {1}", name, count);
+        }
+        public ActionResult Drak(string name, int count)
+        {
+            ViewBag.Message = "blablabla";
+            ViewBag.name = name;
+            ViewBag.count = count;
+            return View();
+        }
+        public ActionResult GetTiger()
+        {
+            var tiger = new Tiger();
+            tiger.color = "white";
+            tiger.age = 5;
+            tiger.area = "India";
+            tiger.extinction = true;
+            tiger.name = "Tigra";
+            return View(tiger);
+        }
+	}
+
+}
---
+++ WebApplication1/WebApplication1/Models/tiger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Tiger 
+    {
+        public string color;
+        public string area;
+        public int age;
+        public string name;
+        public bool extinction;
+
+    }
+}
